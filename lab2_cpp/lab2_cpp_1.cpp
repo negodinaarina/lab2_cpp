97cf7fa32cc7a456f68763086b6610a7d84ac4b8
negodinaarina <negodinaarina24@gmail.com>
2023-09-06 00:26:38 +0300

    Добавьте файлы проекта.

---
+++ lab2_cpp/lab2_cpp_1.cpp
@@ -0,0 +1,45 @@
+﻿#include <iostream>
+using namespace std;
+int main()
+{
+    bool is_prime = true;
+    int x;
+    cout << "Enter number"; cin >> x;
+    // проверка введенного числа на простоту
+    if (x < 2) { is_prime = false; }
+    else {
+        for (int i = 2; i <= sqrt(x); i++) {
+            if (x % i == 0) { is_prime = false; break; }
+        };
+    }
+    // подсчет простых чисел, предшествующих введенному числу
+    int count = 0;
+    bool is_prime_2;
+    for (int i = 1; i <= x; i++) {
+        is_prime_2 = true;
+        if (i < 2) { is_prime_2 = false;}
+        else {
+            for (int j = 2; j <= sqrt(i); j++) {
+                if (i % j == 0) { is_prime_2 = false; break; }
+            };
+        }
+        if (is_prime_2) { count++; };
+    }
+    // проверка простоты номера простого числа
+    bool is_prime_3 = true;
+    if (count < 2) { is_prime_3 = false; }
+    else {
+        for (int i = 2; i <= sqrt(count); i++) {
+            if (count % i == 0) { is_prime_3 = false; break; }
+        };
+    }
+    if (is_prime && is_prime_3) {
+        cout << "Number is super prime\n";
+        cout << count << "\n";
+    }
+    else {
+        cout << "Number is not super prime\n";
+        cout << count << "\n";
+    }
+
+}
