d9b2e250d010a507b809627fea8b66277db121e8
negodinaarina <negodinaarina24@gmail.com>
2023-09-06 23:48:16 +0300

    4rd task completed

---
+++ lab2_cpp/lab2_cpp_4.1.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <ctime>
+#include <string>
+using namespace std;
+int main()
+{
+    // Объявляем переменные, просим ввести количество выстрелов
+    int n; double x; double y; int count;
+    count = 0;
+    cout << "Enter number of shots";
+    cin >> n;
+    // В цикле каждый раз принимает координаты выстрела и
+    // проверяем на попадание в зоны с очками, прибавляем к общему счету count
+    for(int i=0; i < n; i++){
+        cout << "Enter X and Y";
+        cin >> x;
+        cin >> y;
+        if(x*x + y*y <= 1){
+            count += 10;
+        }
+        else if(x*x + y*y <= 4){
+            count += 5;
+        }
+    }
+    cout << "Total score " << count;
+}
---
+++ lab2_cpp/lab2_cpp_5.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <ctime>
+#include <string>
+using namespace std;
+int main()
+{
+    // Объявляем переменные, просим ввести количество выстрелов
+    double x; double y; int count; int score;
+    count, score = 0, 0;
+    // В цикле каждый раз принимает координаты выстрела и
+    // проверяем на попадание в зоны с очками, прибавляем к общему счету count
+    while(score < 50){
+        cout << "Enter X and Y";
+        cin >> x;
+        cin >> y;
+        count += 1;
+        if(x*x + y*y <= 1){
+            score += 10;
+        }
+        else if(x*x + y*y <= 4){
+            score += 5;
+        }
+    }
+    cout << "Total score " << score << endl;
+    cout << "Shots count " << count << endl;
+    if(count <= 9){
+        cout << "You're a pro!";
+    }
+    else if(count <= 20){
+        cout << "You're not bad!";
+    }
+    else{
+        cout << "You're a beginner...";
+    }
+}
